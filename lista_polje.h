4773d043050d8895092fd1053b2d6b255df9d22e
Jasmina Kljunic <jasminakljunic@hotmail.com>
2010-10-22 21:49:29 +0200

    Uploadan kod zadatka

--- lista_polje.h
+++
@@ -1,8 +0,0 @@
-#include "lista_polje.cpp"
-extern int FirstL();
-extern int InsertL();
-extern int LocateL();
-extern int RetrieveL();
-extern int DeleteL();
-extern int RetriveL_MladjiOd18();
-extern int EndL();
